d7c6a7c5e77687e2c349325cef68ba3d5664c31f
SimonGonzalezCepeda <simongonzalez@iesebre.com>
2016-11-03 21:46:03 +0100

    Added more models and migrations. Seeds included

---
+++ database/seeds/CurriculumSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Scool\Curriculum\Database;
+
+use Illuminate\Database\Seeder;
+
+class CurriculumSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
